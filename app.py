13ed6e0308fffd48471385994729902ccfc2353f
REHAGEL REISA <159435168+akucadangan2@users.noreply.github.com>
2025-08-27 17:18:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_login import LoginManager, UserMixin
 from flask_jwt_extended import JWTManager
 from flask_socketio import SocketIO
 
